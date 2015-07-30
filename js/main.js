02ee0b1919449e3a396a735ac7841f8b28fb74c4
Blackstock <charlieblackstock@gmail.com>
2015-07-30 13:53:15 -0600

    first commit with simple jquery added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
+  var $screen = $("#screen");
 
+  function sum (num1, num2) {
+    return num1 + num2;
+  }
+
+  function divide (num1, num2) {
+    return num1 / nume2;
+  }
+
+  function multiply (num1, num2) {
+    return num1 * num2;
+  }
+
+  $(".buttons").find("span").on("click", function() {
+    $number = $(this).text();
+    $("#screen").append($number);
+    if ($number === "+") {
+      $screen = "";
+    }
+  });
 
 
 
